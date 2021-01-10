06161601213a225690f69b43a70b4313cb0fefb4
Ceren Yigin <cerenyigin@gmail.com>
2021-01-10 22:41:15 +0300

    fixed message

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('App running on port ' + port);
-});
+module.exports=function(){
+  console.log('Hello Mars!')
+}
