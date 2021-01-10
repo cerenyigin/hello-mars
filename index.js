0a0087943ce2e1d593df06179025e8116403fe46
Ceren Yigin <cerenyigin@gmail.com>
2021-01-10 22:47:26 +0300

    beta version with languages

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports=function(){
-  console.log('Hello Mars!')
+module.exports=function(lang){
+  if(lang==="tr"){
+    console.log('Merhaba Mars!')
+  }else{
+    console.log('Hello Mars!')
+  }
+ 
 }
